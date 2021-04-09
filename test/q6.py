de12c04f68e0dde3efbe07bd8723a8fec654d810
Mengyu5 <56314224+Mengyu5@users.noreply.github.com>
2021-04-09 14:40:45 -0400

    Update q6.py

--- test/q6.py
+++ test/q6.py
@@ -5,6 +5,6 @@ import glob
 if __name__ == "__main__":
     filename = glob.glob('./*.Rmd')
     f = codecs.open(''.join(filename), "r", "utf-8").read()
-    command1 = re.compile("glm(")
+    command1 = re.compile("glm[(]")
     command2 = re.compile("binomial")
     assert len(command1.findall(f)) + len(command2.findall(f))> 1

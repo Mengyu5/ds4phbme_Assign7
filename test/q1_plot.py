641fa97eb32d8d41af1ea73ebc6f0b8f11c24741
Mengyu5 <56314224+Mengyu5@users.noreply.github.com>
2021-04-09 12:10:15 -0400

    Update q1_plot.py

--- test/q1_plot.py
+++ test/q1_plot.py
@@ -3,7 +3,7 @@ import re
 import glob
 
 if __name__ == "__main__":
-    filename = glob.glob('./*.Rmd')[1]
+    filename = glob.glob('./*.Rmd')
     f = codecs.open(''.join(filename), "r", "utf-8").read()
     command1 = re.compile("plot[(]")
     assert len(command1.findall(f)) > 0

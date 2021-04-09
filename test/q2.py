9e27e7ac9e4bdb1d1deae4a71e79c1ecc36b04dc
Mengyu5 <56314224+Mengyu5@users.noreply.github.com>
2021-04-09 14:38:39 -0400

    Update q2.py

--- test/q2.py
+++ test/q2.py
@@ -3,7 +3,7 @@ import re
 import glob
 
 if __name__ == "__main__":
-    filename = glob.glob('./*.html')
+    filename = glob.glob('./*.Rmd')
     f = codecs.open(''.join(filename), "r", "utf-8").read()
-    command1 = re.compile("5.")
+    command1 = re.compile("predict[(]")
     assert len(command1.findall(f)) > 0

e66447faffa76bf4ad9a8b75c38f0aabb487457f
Mengyu5 <56314224+Mengyu5@users.noreply.github.com>
2021-04-09 14:44:29 -0400

    Update q4.py

--- test/q4.py
+++ test/q4.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     filename = glob.glob('./*.Rmd')
     f = codecs.open(''.join(filename), "r", "utf-8").read()
     command1 = re.compile("lm[(]")
-    assert len(command1.findall(f)) > 0
+    assert len(command1.findall(f)) > 1
